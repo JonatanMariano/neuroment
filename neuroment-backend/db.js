3211699ce7c88aa63295f95d273f62132038001c
Jonatan Mariano da Silva <jonatanmariano12@gmail.com>
2025-09-29 00:15:44 -0300

    Consolidação da Sprint 2 e inicio da Sprint 3

--- neuroment-backend/db.js
+++ neuroment-backend/db.js
@@ -4,10 +4,14 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Render exige SSL, mas ignora certificado self-signed
+  },
 });
 
 module.exports = pool;
+
 
