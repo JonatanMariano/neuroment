b1201152e39496fe1389d071efaec384fa97238a
Jonatan Mariano <jonatanmariano12@gmail.com>
2025-10-31 03:33:50 -0300

    Update App.tsx
    "./routes/Routes"; alterado para "./routes/routes", pois vercel reclamou

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { StoreProvider } from "./stores/StoreProvider";
-import AppRoutes from "./routes/Routes";
+import AppRoutes from "./routes/routes";
 import "./styles/globals.css";
 
 export default function App() {
